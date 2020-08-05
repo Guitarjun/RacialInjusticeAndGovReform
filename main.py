169a5bda7fea018b850699a8fb127ed5ceefca73
Guitarjun <arjunsrivastava01@gmail.com>
2020-08-05 16:57:22 -0700

    added methods to fetch data

--- main.py
+++ main.py
@@ -5,27 +5,41 @@ from pathlib import Path
 import pandas as pd
 
 
-def main():
-    SCOPE = ['https://spreadsheets.google.com/feeds',
-         'https://www.googleapis.com/auth/drive']
-    SERVICE_FILE = 'C:/Users/arjun/Documents/CompSci/racialinjusticegovreform-783aecec559a.json'
-    SPREADSHEET = 'Racial Injustice and Government Reform (Responses)'
+def ask_local():
+    local = input('Would you like to use a local copy of the data? (if no internet connection) (y/n): ')
+    if local.strip() == 'y':
+        return pd.read_csv('Racial Injustice and Government Reform.csv')
+    else:
+        print('Fetching data...')
+        return load_data()
 
+
+def load_data():
+    SCOPE = ['https://spreadsheets.google.com/feeds',
+             'https://www.googleapis.com/auth/drive']
+    SERVICE_FILE = 'racialinjusticegovreform-783aecec559a.json'
+    SPREADSHEET = 'Racial Injustice and Government Reform (Responses)'
     credentials = service_account.Credentials.from_service_account_file(
         SERVICE_FILE, scopes=SCOPE)
-
     gc = gspread.authorize(credentials)
+    workbook = gc.open(SPREADSHEET)
+    sheet = workbook.sheet1
+    data = pd.DataFrame(sheet.get_all_records())
+    return data
+
+def update_columns(data):
+
+
+def main():
+
+    data = ask_local()
+    """
     print("The following sheets are available")
     for sheet in gc.openall():
         print("{} - {}".format(sheet.title, sheet.id))
-    workbook = gc.open(SPREADSHEET)
+    """
 
-    sheet = workbook.sheet1
-
-    data = pd.DataFrame(sheet.get_all_records())
-
-    print(data.columns)
-
+    print(type(data))
 
 
 if __name__ == '__main__':
